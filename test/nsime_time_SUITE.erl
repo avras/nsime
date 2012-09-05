182ae08927864b18358183e15235f29360e0791e
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-09-06 00:05:46 +0530

    Added GNU GPLv3 to all files

--- test/nsime_time_SUITE.erl
+++ test/nsime_time_SUITE.erl
@@ -1,9 +1,20 @@
 %%
-%% %CopyrightBegin%
-%% 
-%% Copyright Saravanan Vijayakumaran 2012. All Rights Reserved.
-%% 
-%% %CopyrightEnd%
+%%  Copyright (C) 2012 Saravanan Vijayakumaran <sarva.v@gmail.com>
+%%
+%%  This file is part of nsime.
+%%
+%%  nsime is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  nsime is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with nsime.  If not, see <http://www.gnu.org/licenses/>.
 %%
 
 %% Purpose : Test module for nsime_time
