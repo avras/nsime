dd800204d2349edeeeff325cd0a37c6de9b86851
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-09-15 20:27:53 +0530

    More code in nsime_ipv4_static_routing

--- src/nsime_ipv4_static_routing.erl
+++ src/nsime_ipv4_static_routing.erl
@@ -33,8 +33,7 @@
          notify_interface_up/2, notify_interface_down/2, notify_add_address/3,
          notify_remove_address/3, set_ipv4_protocol/2, add_network_route/5,
          add_network_route/6, add_host_route/4, add_host_route/5,
-         set_default_route/3, set_default_route/4, get_default_route/1,
-         get_network_routes/1, get_route/2, remove_route/2]).
+         set_default_route/4, get_network_routes/1]).
 
 create() ->
     {ok, Pid} = gen_server:start(?MODULE, [], []),
@@ -43,18 +42,188 @@ create() ->
 destroy(RoutingPid) ->
     gen_server:call(RoutingPid, terminate).
 
+route_input(
+    RoutingPid,
+    Packet,
+    Ipv4Header,
+    IngressNetdevice,
+    UnicastForwardCallback,
+    MulticastForwardCallback,
+    LocalDeliverCallback,
+    ErrorCallback
+) ->
+    gen_server:call(RoutingPid, {route_input,
+                                 Packet,
+                                 Ipv4Header,
+                                 IngressNetdevice,
+                                 UnicastForwardCallback,
+                                 MulticastForwardCallback,
+                                 LocalDeliverCallback,
+                                 ErrorCallback
+                                }).
+
+route_output(
+    RoutingPid,
+    Packet,
+    Ipv4Header,
+    OutputNetdevice
+) ->
+    gen_server:call(RoutingPid, {route_output,
+                                 Packet,
+                                 Ipv4Header,
+                                 OutputNetdevice
+                                }).
+
+notify_interface_up(RoutingPid, InterfaceIndex) ->
+    gen_server:call(RoutingPid, {notify_interface_up, InterfaceIndex}).
+
+notify_interface_down(RoutingPid, InterfaceIndex) ->
+    gen_server:call(RoutingPid, {notify_interface_down, InterfaceIndex}).
+
+notify_add_address(RoutingPid, InterfaceIndex, InterfaceAddress) ->
+    gen_server:call(RoutingPid, {notify_add_address,
+                                 InterfaceIndex,
+                                 InterfaceAddress
+                                }).
+
+notify_remove_address(RoutingPid, InterfaceIndex, InterfaceAddress) ->
+    gen_server:call(RoutingPid, {notify_remove_address,
+                                 InterfaceIndex,
+                                 InterfaceAddress
+                                }).
+
 set_ipv4_protocol(RoutingPid, Ipv4ProtocolPid) ->
     gen_server:call(RoutingPid, {set_ipv4_protocol, Ipv4ProtocolPid}).
 
+add_network_route(
+    RoutingPid,
+    NetworkAddress,
+    NetworkMask,
+    InterfaceIndex,
+    Metric
+) ->
+    gen_server:call(RoutingPid, {add_network_route,
+                                 NetworkAddress,
+                                 NetworkMask,
+                                 InterfaceIndex,
+                                 Metric
+                                }).
+
+add_network_route(
+    RoutingPid,
+    NetworkAddress,
+    NetworkMask,
+    NextHopAddress,
+    InterfaceIndex,
+    Metric
+) ->
+    gen_server:call(RoutingPid, {add_network_route,
+                                 NetworkAddress,
+                                 NetworkMask,
+                                 NextHopAddress,
+                                 InterfaceIndex,
+                                 Metric
+                                }).
+
+add_host_route(RoutingPid, DestinationAddress, InterfaceIndex, Metric) ->
+    add_network_route(
+        RoutingPid, 
+        DestinationAddress,
+        nsime_ipv4_mask:get_ones(),
+        InterfaceIndex,
+        Metric
+    ).
+
+add_host_route(
+    RoutingPid,
+    DestinationAddress,
+    NextHopAddress,
+    InterfaceIndex,
+    Metric
+) ->
+    add_network_route(
+        RoutingPid, 
+        DestinationAddress,
+        nsime_ipv4_mask:get_ones(),
+        NextHopAddress,
+        InterfaceIndex,
+        Metric
+    ).
+
+set_default_route(RoutingPid, NextHopAddress, InterfaceIndex, Metric) ->
+    add_network_route(
+        RoutingPid,
+        nsime_ipv4_adddress:get_zero(),
+        nsime_ipv4_mask:get_zero(),
+        NextHopAddress,
+        InterfaceIndex,
+        Metric
+    ).
+
+get_network_routes(RoutingPid) ->
+    gen_server:call(RoutingPid, get_network_routes).
+
 init([]) ->
     RoutingState = #nsime_ipv4_static_routing_state{},
-    {ok, RoutingState};
+    {ok, RoutingState}.
 
 handle_call({set_ipv4_protocol, Ipv4ProtocolPid}, _From, RoutingState) ->
     NewRoutingState = RoutingState#nsime_ipv4_static_routing_state{
         ipv4_protocol = Ipv4ProtocolPid
     },
     {reply, ok, NewRoutingState};
+
+handle_call(
+    {
+        add_network_route,
+        NetworkAddress,
+        NetworkMask,
+        InterfaceIndex,
+        Metric
+    },
+    _From,
+    RoutingState
+) ->
+    NetworkRoutes = RoutingState#nsime_ipv4_static_routing_state.network_routes,
+    Route = nsime_ipv4_routing_table_entry:create_network_route(
+        NetworkAddress,
+        NetworkMask,
+        InterfaceIndex
+    ),
+    NewNetworkRoutes = [Route | NewNetworkRoutes],
+    NewRoutingState = RoutingState#nsime_ipv4_static_routing_state{
+        network_routes = NewNetworkRoutes
+    },
+    {reply, ok, NewRoutingState};
+
+handle_call(
+    {
+        add_network_route,
+        NetworkAddress,
+        NetworkMask,
+        NextHopAddress,
+        InterfaceIndex,
+        Metric
+    },
+    _From,
+    RoutingState
+) ->
+    NetworkRoutes = RoutingState#nsime_ipv4_static_routing_state.network_routes,
+    Route = nsime_ipv4_routing_table_entry:create_network_route(
+        NetworkAddress,
+        NetworkMask,
+        NextHopAddress,
+        InterfaceIndex
+    ),
+    NewNetworkRoutes = [Route | NewNetworkRoutes],
+    NewRoutingState = RoutingState#nsime_ipv4_static_routing_state{
+        network_routes = NewNetworkRoutes
+    },
+    {reply, ok, NewRoutingState};
+
+handle_call(get_network_routes, _From, RoutingState) ->
+    NetworkRoutes = RoutingState#nsime_ipv4_static_routing_state.network_routes,
+    {reply, NetworkRoutes, RoutingState};
 
 handle_call(terminate, _From, RoutingState) ->
     {stop, normal, stopped, RoutingState}.
