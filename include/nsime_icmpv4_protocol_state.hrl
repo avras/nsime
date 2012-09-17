684d1f58e0e3059df76a77b2ee448c6bd50a3b93
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-09-17 12:32:51 +0530

    Added nsime_icmpv4_protocol_state

---
+++ include/nsime_icmpv4_protocol_state.hrl
@@ -0,0 +1,27 @@
+%%
+%%  Copyright (C) 2012 Saravanan Vijayakumaran <sarva.v@gmail.com>
+%%
+%%  This file is part of nsime.
+%%
+%%  nsime is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  nsime is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with nsime.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
+%% Purpose : ICMPv4 protocol state record
+%% Author : Saravanan Vijayakumaran
+
+-record(nsime_icmpv4_protocol_state,
+        {
+          node                      :: pid(),
+          ipv4_down_target          :: nsime_callback()
+        }).
