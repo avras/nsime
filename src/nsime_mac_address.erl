595e902310fb18e6fd668ebc9fc90298a84bf588
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-10-11 08:07:30 +0530

    Added MAC address allocator module nsime_mac_address

---
+++ src/nsime_mac_address.erl
@@ -0,0 +1,74 @@
+%%
+%%  Copyright (C) 2012 Saravanan Vijayakumaran <sarva.v@gmail.com>
+%%
+%%  This file is part of nsime.
+%%
+%%  nsime is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  nsime is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with nsime.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
+%% Purpose : MAC address allocator module
+%% Author : Saravanan Vijayakumaran
+%% Description: Implementation of singleton MAC address allocator
+
+-module(nsime_mac_address).
+-author("Saravanan Vijayakumaran").
+
+-include("nsime_mac_address_state.hrl").
+
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-export([start/0, stop/0, allocate/0]).
+
+start() ->
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:call(?MODULE, terminate).
+
+init([]) ->
+    AllocatorState = #nsime_mac_address_state{},
+    {ok, AllocatorState}.
+
+allocate() ->
+    case lists:member(nsime_mac_address, erlang:registered()) of
+        true ->
+            gen_server:call(?MODULE, allocate);
+        false ->
+            nsime_mac_address:start(),
+            gen_server:call(?MODULE, allocate)
+    end.
+
+handle_call(allocate, _From, AllocatorState) ->
+    NewAddress = AllocatorState#nsime_mac_address_state.current_address + 1,
+    NewAllocatorState = AllocatorState#nsime_mac_address_state{
+        current_address = NewAddress
+    },
+    {reply, <<NewAddress:48>>, NewAllocatorState};
+
+handle_call(terminate, _From, AllocatorState) ->
+    {stop, normal, stopped, AllocatorState}.
+
+handle_cast(_Request, AllocatorState) ->
+    {noreply, AllocatorState}.
+
+handle_info(_Request, AllocatorState) ->
+    {noreply, AllocatorState}.
+
+terminate(_Reason, _AllocatorState) ->
+    ok.
+
+code_change(_OldVersion, AllocatorState, _Extra) ->
+    {ok, AllocatorState}.
---
+++ test/nsime_mac_address_SUITE.erl
@@ -0,0 +1,76 @@
+%%
+%%  Copyright (C) 2012 Saravanan Vijayakumaran <sarva.v@gmail.com>
+%%
+%%  This file is part of nsime.
+%%
+%%  nsime is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  nsime is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with nsime.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
+%% Purpose : Test module for nsime_mac_address
+%% Author : Saravanan Vijayakumaran
+
+-module(nsime_mac_address_SUITE).
+-author("Saravanan Vijayakumaran").
+
+-compile(export_all).
+
+-include("ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-include("nsime_mac_address_state.hrl").
+
+all() -> [
+            test_start_stop,
+            test_allocate,
+            test_cast_info_codechange
+         ].
+
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(Config) ->
+    Config.
+
+test_start_stop(_) ->
+    nsime_mac_address:start(),
+    ?assert(lists:member(nsime_mac_address, erlang:registered())),
+    ?assertEqual(nsime_mac_address:stop(), stopped),
+    ?assertNot(lists:member(nsime_mac_address, erlang:registered())).
+
+test_allocate(_) ->
+    nsime_mac_address:start(),
+    ?assert(lists:member(nsime_mac_address, erlang:registered())),
+    ?assertEqual(nsime_mac_address:allocate(), <<1:48>>),
+    ?assertEqual(nsime_mac_address:allocate(), <<2:48>>),
+    ?assertEqual(nsime_mac_address:allocate(), <<3:48>>),
+    ?assertEqual(nsime_mac_address:stop(), stopped),
+    ?assertNot(lists:member(nsime_mac_address, erlang:registered())),
+
+    ?assertEqual(nsime_mac_address:allocate(), <<1:48>>),
+    ?assertEqual(nsime_mac_address:allocate(), <<2:48>>),
+    ?assertEqual(nsime_mac_address:allocate(), <<3:48>>),
+    ?assert(lists:member(nsime_mac_address, erlang:registered())),
+    ?assertEqual(nsime_mac_address:stop(), stopped),
+    ?assertNot(lists:member(nsime_mac_address, erlang:registered())).
+
+test_cast_info_codechange(_) ->
+    nsime_mac_address:start(),
+    Pid = erlang:whereis(nsime_mac_address),
+    ?assert(erlang:is_pid(Pid)),
+    ?assert(lists:member(nsime_mac_address, erlang:registered())),
+    gen_server:cast(nsime_mac_address, junk),
+    Pid ! junk,
+    nsime_mac_address:code_change(junk, junk, junk),
+    ?assertEqual(nsime_mac_address:stop(), stopped).
