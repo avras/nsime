489c79b6e56a576957f702245da931e8b7128d17
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-11-14 12:04:40 +0530

    Fixes to nsime_ipv4_interface_address_SUITE

--- test/nsime_ipv4_interface_address_SUITE.erl
+++ test/nsime_ipv4_interface_address_SUITE.erl
@@ -33,8 +33,7 @@
 
 all() -> [
             test_creation_shutdown,
-            test_set_get_components,
-            test_cast_info_codechange
+            test_set_get_components
          ].
 
 
@@ -45,106 +44,46 @@ end_per_suite(Config) ->
     Config.
 
 test_creation_shutdown(_) ->
-    AddressPid1 = nsime_ipv4_interface_address:create(),
-    ?assert(is_pid(AddressPid1)),
-    AddressState = #nsime_ipv4_interface_address_state{
-        local_address = nsime_ipv4_address:get_any(),
-        broadcast_address = nsime_ipv4_address:get_broadcast()
-    },
-
-    AddressPid2 = nsime_ipv4_interface_address:create(AddressState),
-    ?assert(is_pid(AddressPid2)),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_local_address(AddressPid2),
-        nsime_ipv4_address:get_any()
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_broadcast_address(AddressPid2),
-        nsime_ipv4_address:get_broadcast()
-    ),
+    AddressState1 = nsime_ipv4_interface_address:create(),
+    ?assert(is_record(AddressState1, nsime_ipv4_interface_address_state)),
 
     Address = {10, 107, 1, 1},
     Mask = {255, 255, 255, 0},
-    AddressPid3 = nsime_ipv4_interface_address:create(Address, Mask),
-    ?assert(is_pid(AddressPid3)),
+    AddressState2 = nsime_ipv4_interface_address:create(Address, Mask),
+    ?assert(is_record(AddressState2, nsime_ipv4_interface_address_state)),
     ?assertEqual(
-        nsime_ipv4_interface_address:get_local_address(AddressPid3),
+        nsime_ipv4_interface_address:get_local_address(AddressState2),
         Address
     ),
     ?assertEqual(
-        nsime_ipv4_interface_address:get_mask(AddressPid3),
+        nsime_ipv4_interface_address:get_mask(AddressState2),
         Mask
     ),
     ?assertEqual(
-        nsime_ipv4_interface_address:get_broadcast_address(AddressPid3),
+        nsime_ipv4_interface_address:get_broadcast_address(AddressState2),
         {10, 107, 1, 255}
-    ),
-
-    ?assertEqual(nsime_ipv4_interface_address:destroy(AddressPid3), stopped),
-    ?assertEqual(nsime_ipv4_interface_address:destroy(AddressPid2), stopped),
-    ?assertEqual(nsime_ipv4_interface_address:destroy(AddressPid1), stopped).
+    ).
 
 test_set_get_components(_) ->
-    AddressPid1 = nsime_ipv4_interface_address:create(),
-    ?assert(is_pid(AddressPid1)),
+    AddressState1 = nsime_ipv4_interface_address:create(),
+    ?assert(is_record(AddressState1, nsime_ipv4_interface_address_state)),
     Address = {10, 107, 1, 1},
-    ?assertEqual(
-        nsime_ipv4_interface_address:set_local_address(
-            AddressPid1,
-            Address
-        ),
-        ok
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_local_address(AddressPid1),
-        Address
-    ),
+    AddressState2 = nsime_ipv4_interface_address:set_local_address(AddressState1, Address),
+    ?assert(is_record(AddressState2, nsime_ipv4_interface_address_state)),
+    ?assertEqual(nsime_ipv4_interface_address:get_local_address(AddressState2), Address),
     BroadcastAddress = {10, 107, 1, 255},
-    ?assertEqual(
-        nsime_ipv4_interface_address:set_broadcast_address(
-            AddressPid1,
-            BroadcastAddress
-        ),
-        ok
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_broadcast_address(AddressPid1),
-        BroadcastAddress
-    ),
+    AddressState3 = nsime_ipv4_interface_address:set_broadcast_address(AddressState2, BroadcastAddress),
+    ?assert(is_record(AddressState3, nsime_ipv4_interface_address_state)),
+    ?assertEqual(nsime_ipv4_interface_address:get_broadcast_address(AddressState3), BroadcastAddress),
     Mask = {255, 255, 255, 0},
-    ?assertEqual(
-        nsime_ipv4_interface_address:set_mask(
-            AddressPid1,
-            Mask
-        ),
-        ok
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_mask(AddressPid1),
-        Mask
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:set_address_scope(
-            AddressPid1,
-            link
-        ),
-        ok
-    ),
-    ?assertEqual(
-        nsime_ipv4_interface_address:get_address_scope(AddressPid1),
-        link
-    ),
-    ?assertNot(nsime_ipv4_interface_address:is_secondary(AddressPid1)),
-    ?assertEqual(nsime_ipv4_interface_address:set_secondary(AddressPid1), ok),
-    ?assert(nsime_ipv4_interface_address:is_secondary(AddressPid1)),
-    ?assertEqual(nsime_ipv4_interface_address:set_primary(AddressPid1), ok),
-    ?assertNot(nsime_ipv4_interface_address:is_secondary(AddressPid1)),
-    ?assertEqual(nsime_ipv4_interface_address:destroy(AddressPid1), stopped).
-
-test_cast_info_codechange(_) ->
-    AddressPid = nsime_ipv4_interface_address:create(),
-    ?assert(is_pid(AddressPid)),
-    gen_server:cast(AddressPid, junk),
-    AddressPid ! junk,
-    nsime_ipv4_interface_address:code_change(junk, junk, junk),
-    ?assertEqual(nsime_ipv4_interface_address:destroy(AddressPid), stopped).
+    AddressState4 = nsime_ipv4_interface_address:set_mask(AddressState3, Mask),
+    ?assert(is_record(AddressState4, nsime_ipv4_interface_address_state)),
+    ?assertEqual(nsime_ipv4_interface_address:get_mask(AddressState4), Mask),
+    AddressState5 = nsime_ipv4_interface_address:set_address_scope(AddressState4, link),
+    ?assert(is_record(AddressState5, nsime_ipv4_interface_address_state)),
+    ?assertEqual(nsime_ipv4_interface_address:get_address_scope(AddressState5), link),
+    ?assertNot(nsime_ipv4_interface_address:is_secondary(AddressState5)),
+    AddressState6 = nsime_ipv4_interface_address:set_secondary(AddressState5),
+    ?assert(nsime_ipv4_interface_address:is_secondary(AddressState6)),
+    AddressState7 = nsime_ipv4_interface_address:set_primary(AddressState6),
+    ?assertNot(nsime_ipv4_interface_address:is_secondary(AddressState7)).
