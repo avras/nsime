3101ddad4fd502e49114cab638f7dd29b151589c
Saravanan Vijayakumaran <sarva.v@gmail.com>
2012-11-14 20:18:58 +0530

    Replacing run with parallel_run in udp_cs_pairs

--- examples/src/udp_cs_pairs.erl
+++ examples/src/udp_cs_pairs.erl
@@ -72,7 +72,7 @@ start([NumPairsString]) ->
     ),
 
 
-    nsime_simulator:run(),
+    nsime_simulator:parallel_run(),
     nsime_ptp_helper:destroy(PtpHelperPid),
     nsime_ipv4_address_helper:destroy(AddressHelperPid),
     nsime_simulator:stop().
